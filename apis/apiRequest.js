03aeeec1def726ef0c2eb226ddfbaafa170487b2
raveen singh <raveen.singh@unthinkable.co>
2021-05-22 17:53:31 +0530

    removed commented code

--- apis/apiRequest.js
+++ apis/apiRequest.js
@@ -1,10 +1,6 @@
 import superagent from 'superagent';
-// import getConfig from 'next/config';
-
-// const { publicRuntimeConfig } = getConfig();
 
 export default function apiRequest() {
-  // const { api } = publicRuntimeConfig;
   const _url = 'https://intense-tor-76305.herokuapp.com/merchants';
 
   const request = superagent('get', _url);
